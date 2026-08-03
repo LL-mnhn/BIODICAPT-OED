ccf6014d5497c1ff6be75e6cf4668613dddaa62c
LL-mnhn <loic.lehnhoff@mnhn.fr>
2026-08-03 11:49:45 +0200

    (fix): modified script.sh and added .Rprofile to repo

--- script.sh
+++ script.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-#SBATCH --job-name=TEST
+#SBATCH --job-name=BIODICAPT
 #SBATCH --partition=std
 #SBATCH --mem=32G
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=5
-#SBATCH --time=00-06:00:00
+#SBATCH --cpus-per-task=3 # number of chains to run (NOT number of K-folds)
+#SBATCH --time=00-03:00:00
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
 ## Load software modules
 module load R/4.6.1/gcc-openmpi
+cd "$SLURM_SUBMIT_DIR"
 
 ## Stop if there is an error
 set -e
